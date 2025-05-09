e9e322c1556b64e46bdf3dff5583363bc0f20023
Mariia Moskovko <39185269+Maria0603@users.noreply.github.com>
2025-05-09 13:18:02 +0200

    Update MyPlant.test.tsx

--- tests/pages/MyPlant.test.tsx
+++ tests/pages/MyPlant.test.tsx
@@ -55,7 +55,7 @@ describe("MyPlants - Add Type Creation", () => {
     fireEvent.click(screen.getByText("Add new type"));
 
     // Fill out the form
-    fireEvent.change(screen.getByLabelText(/^Type$/i), {
+    fireEvent.change(screen.getByLabelText("Type"), {
       target: { value: "Basil" },
     });
     fireEvent.change(screen.getByLabelText(/Watering Frequency/i), {
