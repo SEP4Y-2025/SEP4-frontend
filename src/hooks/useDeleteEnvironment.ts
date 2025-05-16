de7a94c001dd27ba1cab443576b841e2cfde2985
Mariia Moskovko <39185269+Maria0603@users.noreply.github.com>
2025-05-16 09:50:42 +0200

    fix delete environment

--- src/hooks/useDeleteEnvironment.ts
+++ src/hooks/useDeleteEnvironment.ts
@@ -1,6 +1,8 @@
 import { use, useState } from "react";
 import axios from "axios";
 
+const BASE_URL = process.env.REACT_APP_BACKEND_URL || "http://localhost:8000";
+
 export function useDeleteEnvironment() {
     const [errorDelete, setErrorDelete] = useState<string | null>(null);
     const [successDelete, setSuccessDelete] = useState<string | null>(null);
@@ -9,9 +11,10 @@ export function useDeleteEnvironment() {
         setErrorDelete(null);
         setSuccessDelete(null);
         try {
-            const response = await axios.delete("http://localhost:8000/environments/" + environmentId);
-            setSuccessDelete(response.data?.message || "Password changed successfully");
+            const response = await axios.delete(`${BASE_URL}/environments/${environmentId}`);
+            setSuccessDelete(response.data?.message || "Environment deleted successfully");
         } catch (err: any) {
+            console.error("Error deleting environment:", err.response?.data.message);
             setErrorDelete(
                 err.response?.data?.error ||
                 err.response?.data?.message ||
