2f0ead4e6741c3b1ecb327918cc6810e956ee604
Katarzyna Olejarczyk <147693001+katiekasia@users.noreply.github.com>
2025-05-17 15:33:36 +0200

    working adding a plant

--- src/hooks/useAddPlantPot.ts
+++ src/hooks/useAddPlantPot.ts
@@ -5,6 +5,7 @@ import {
   PlantPotResponse,
 } from "../types/addPlantPotApiTypes";
 
+
 const BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:8000";
 
 export const useAddPlantPot = (onSuccess?:() => void) => {
@@ -12,17 +13,19 @@ export const useAddPlantPot = (onSuccess?:() => void) => {
   const [error, setError] = useState<Error | null>(null);
 
   const addPlantPot = async (
+    env_id: string,
     pot: AddPlantPotRequest
   ): Promise<PlantPotResponse | null> => {
     setLoading(true);
     setError(null);
     try {
-      const response = await axios.post(`${BASE_URL}/pots`, pot);
+      const response = await axios.post(`${BASE_URL}/environments/${env_id}/pots`, pot);
       onSuccess && onSuccess();
       return response.data;
-    } catch (err) {
-      setError(err as Error);
-      return null;
+    } catch (err: any) {
+       const backendError = err.response?.data?.detail || "Unexpected error";
+      setError(new Error(backendError));
+        throw new Error(backendError); 
     } finally {
       setLoading(false);
     }
--- src/pages/AddPlant.tsx
+++ src/pages/AddPlant.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
-
+import { useGetTypesByEnvironment } from "../hooks/useGetTypesByEnvironment";
+import { useEffect } from "react";
+import { useEnvironmentCtx } from "../contexts/EnvironmentContext";
 import { AddPlantPotRequest } from "../types/addPlantPotApiTypes";
 import "./AddPlant.css";
 import { useAddPlantPot } from "../hooks/useAddPlantPot";
@@ -8,12 +10,14 @@ import { toast } from "react-toastify";
 import { StyledAddPlantModal, StyledInputGroup, StyledModalBody, StyledModalContent, StyledModalFooter, StyledModalHeader } from "../Styles/pages/AddPlant.style";
 
 const AddPlant: React.FC = () => {
-  const { environmentId, plantTypeId, typeName } = useParams<{
-    environmentId: string;
-    plantTypeId: string;
+  const { typeName } = useParams<{
     typeName: string;
   }>();
-
+  const {environmentID} = useEnvironmentCtx();
+  
+  const { types} = useGetTypesByEnvironment(environmentID);
+  const plantTypeId = types.find((type) => type.name === typeName)?._id ?? "";
+  
   const navigate = useNavigate();
   const [plantName, setPlantName] = useState("");
   const [potId, setPotId] = useState("");
@@ -31,16 +35,28 @@ const AddPlant: React.FC = () => {
       const request: AddPlantPotRequest = {
         pot_id: potId,
         plant_pot_label: plantName,
-        plant_type_id: plantTypeId!,
+        plant_type_id: plantTypeId,
       };
-
-      await addPlantPot(request);
+      console.log("Request payload:", request);
+      await addPlantPot(environmentID, request);
+      toast.success("Plant added successfully");
       navigate("/plants");
     } catch (e: any) {
-      console.error(e);
-      setError("Failed to add plant. Please try again.");
+    console.error("Add plant error:", e.message);
+
+    if (e.message.includes("Timeout waiting for Arduino response")) {
+      toast.error("Failed to connect to Arduino. Make sure it's connected.");
+    } 
+    else if(e.message.includes("Unknown or unregistered Arduino")) {
+      toast.error("Unregistered Arduino. Please register it first.");
     }
-  };
+      else {
+      toast.error(e.message || "Failed to add plant. Please try again.");
+    }
+
+    //setError(e.message);
+  }
+};
 
   const handleCancel = () => {
     navigate(-1);
@@ -63,7 +79,9 @@ const AddPlant: React.FC = () => {
               className="input"
               placeholder="Enter device ID"
               value={potId}
-              onChange={(e) => setPotId(e.target.value)}
+              onChange={(e) => setPotId(e.target.value)
+               
+              }
             />
           </StyledInputGroup>
 
@@ -73,8 +91,9 @@ const AddPlant: React.FC = () => {
               className="input"
               placeholder="Enter plant name"
               value={plantName}
-              onChange={(e) => setPlantName(e.target.value)}
-            />
+              onChange={(e) => setPlantName(e.target.value)
+            }
+              />
           </StyledInputGroup>
 
           <StyledInputGroup>
